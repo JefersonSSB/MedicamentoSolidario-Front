bbe079285e0c51ed2b01cbe66c7f7a3761690df9
Jeferson S Santos <jefersonsilvasantos_@hotmail.com>
2020-03-29 21:42:35 -0300

    Fake Login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,17 +1,19 @@
 import { FormGroup, FormControl } from '@angular/forms';
 import { Input, Component, Output, EventEmitter } from '@angular/core';
 import { LoginService } from './login.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
-const formData = new FormData();
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent  {
+export class LoginComponent {
 
-  constructor (private loginService: LoginService){}
+
+  constructor(private loginService: LoginService, private _snackBar: MatSnackBar, public router: Router) { }
 
   form: FormGroup = new FormGroup({
     username: new FormControl(''),
@@ -19,9 +21,35 @@ export class LoginComponent  {
     grant_type: new FormControl('password')
   });
 
+
+  login() {
+
+
+    if (this.form.controls.username.value === "admin" && this.form.controls.password.value === "admin") {
+      localStorage.setItem("isAuth", "true");
+      this.openSnackBar('Logado com Sucesso !', 'X')
+      this.router.navigate(['/']);
+    }
+    else {
+      this.openSnackBar('Login ou Senha Invalidos !', 'X')
+    }
+
+
+  }
+
+  public auth(): boolean {
+    return localStorage.getItem('isAuth') === "false";
+  };
+
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action, {
+      duration: 2000,
+    });
+  }
+
   submit() {
 
-    this.loginService.login(this.form.value).subscribe(data => {console.log(data); });
+    this.loginService.login(this.form.value).subscribe(data => { console.log(data); });
 
     if (this.form.valid) {
       console.log(this.form.value);
